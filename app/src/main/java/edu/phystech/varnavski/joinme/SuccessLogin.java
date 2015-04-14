b9928120c9e7b0076b3704faa0c50ed0482f6094
Nipel-Crumple <warlen94@mail.ru>
2015-04-14 14:32:04 +0300

    Categories page release

--- app/src/main/java/edu/phystech/varnavski/joinme/SuccessLogin.java
+++ app/src/main/java/edu/phystech/varnavski/joinme/SuccessLogin.java
@@ -1,7 +1,9 @@
 package edu.phystech.varnavski.joinme;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by Johnny D on 11.04.2015.
@@ -12,5 +14,21 @@ public class SuccessLogin extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.success);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Typeface font = Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
+                TextView food = (TextView)findViewById(R.id.food_header);
+                TextView sport = (TextView)findViewById(R.id.sport_header);
+                TextView teaching = (TextView)findViewById(R.id.teaching_header);
+                TextView movie = (TextView)findViewById(R.id.movie_header);
+                TextView theater = (TextView)findViewById(R.id.theater_header);
+                food.setTypeface(font);
+                sport.setTypeface(font);
+                teaching.setTypeface(font);
+                movie.setTypeface(font);
+                theater.setTypeface(font);
+            }
+        }).start();
     }
 }
--- app/src/main/java/edu/phystech/varnavski/joinme/signin/SignInActivity.java
+++ app/src/main/java/edu/phystech/varnavski/joinme/signin/SignInActivity.java
@@ -3,6 +3,7 @@ package edu.phystech.varnavski.joinme.signin;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +16,7 @@ import edu.phystech.varnavski.joinme.processors.LoginProcessor;
 /**
  * Created by Johnny D on 11.04.2015.
  */
-public class SignInActivity extends Activity{
+public class SignInActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
